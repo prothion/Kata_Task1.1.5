6e2d5eed39e9e63cd1f8fceeff9792b8d4eea035
prothion <derechinvd@gmail.com>
2022-12-11 15:59:02 +0300

    Java pre-project. Задача 1.1.4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,20 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+    private final static UserService userService = new UserServiceImpl();
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        userService.createUsersTable();
+        userService.saveUser("Вадим", "Дрз", (byte) 78);
+        userService.saveUser("Райора", "Чхх", (byte) 74);
+        userService.saveUser("Дмитрий", "Ктр", (byte) 59);
+        userService.saveUser("Владимир", "Прп", (byte) 74);
+        userService.removeUserById(3);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,46 @@
 package jm.task.core.jdbc.util;
 
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static Connection conn = null;
+    private static Util instance = null;
+    private Util() {
+        try {
+            if (null == conn || conn.isClosed()) {
+                Properties properties = getProperties();
+                conn = DriverManager.getConnection(properties.getProperty("db.url"), properties.getProperty("db.username"), properties.getProperty("db.password"));
+            }
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static Util getInstance() {
+        if (null == instance) {
+            instance = new Util();
+        }
+        return instance;
+    }
+    public Connection getConnection() {
+        return conn;
+    }
+    private static Properties getProperties() throws IOException {
+        Properties properties = new Properties();
+        try (InputStream openStr = Files.newInputStream(Paths.get(Util.class.getResource("/prop.properties").toURI()))) {
+            properties.load(openStr);
+            return properties;
+        } catch (IOException | URISyntaxException e) {
+            throw new IOException("Не найден properties файл!", e);
+        }
+    }
 }
