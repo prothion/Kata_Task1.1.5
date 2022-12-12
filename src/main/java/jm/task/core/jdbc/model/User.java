99a04afc3522926ea57e3c27384f6c3058b692a3
Vadim <109858454+prothion@users.noreply.github.com>
2022-12-12 07:07:16 +0300

    Update User.java

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,25 +1,24 @@
 package jm.task.core.jdbc.model;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
-@Table
+@Entity
+@Table (name = "user")
 public class User {
     @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
     private String name;
 
-    @Column
+    @Column (name = "last_name")
     private String lastName;
 
     @Column
     private Byte age;
 
     public User() {
-
     }
 
     public User(String name, String lastName, Byte age) {
